983261c5b513aafbc0d8a54d36ea8a705905e52f
noel <noelnolife@gmail.com>
2025-01-30 08:18:45 +0700

    add interface

--- resources/js/types/global.d.ts
+++ resources/js/types/global.d.ts
@@ -8,14 +8,31 @@ declare global {
         axios: AxiosInstance;
     }
 
+    interface VideoGame {
+        id: number;
+        title: string;
+        developer: string?;
+        publisher: string?;
+        release_date: string?;
+        genres : string?;
+        product_rating: string?;
+        user_score: number?;
+        platforms: Array<string>?;
+      }
+
     interface ModalComponentProps {
         id : string,
-        vg : any
+        vg : VideoGame
     }
 
     interface AddVGComponentProps {
         className : string,
         disabled : boolean
+    }
+
+    interface Pagination {
+        data : VideoGame[],
+        links : {url: string?, label: string, active:boolean}[];
     }
 
     /* eslint-disable no-var */
